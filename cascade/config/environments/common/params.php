53a1b03fc16772327751154449f32bac26195bcf
Jacob Morrison <jmorrison@psesd.org>
2013-12-05 11:29:29 -0800

    -n Moving cascade to its own unique namespace

---
+++ cascade/config/environments/common/params.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * ./app/config/environments/common/params.php
+ *
+ * @author Jacob Morrison <jacob@infinitecascade.com>
+ * @package cascade
+ */
+
+return array(
+	'siteName' => 'Cascade',
+	'sessionExpiration' => 3600,
+	'defaultCountry' => 'US',
+	'defaultSubnationalDivision' => null,
+	'migrationAliases' => ['@app/migrations'],
+	// site look
+	'logoLogin' => "/themes/ic/img/cascade-logo-450.png",
+	'logoSmall' => "/themes/ic/img/cascade-logo-75.png"
+);
+?>
---
+++ cascade/views/object/view.php
@@ -0,0 +1,81 @@
+<?php
+use Yii;
+
+use infinite\helpers\Html;
+use yii\bootstrap\Nav;
+use infinite\web\bootstrap\SubNavBar;
+
+$baseInstructions = [];
+$baseInstructions['objectId'] = $object->primaryKey;
+$refreshable = [
+	'baseInstructions' => $baseInstructions,
+	'url' => Html::url('app/refresh'),
+	'data' => [Yii::$app->request->csrfVar => Yii::$app->request->csrfToken]
+];
+$this->bodyHtmlOptions['data-refreshable'] = json_encode($refreshable);
+$js = [];
+$js[] = "\$('body').scrollspy({ target: '#object-dashboard-navbar', 'offset': 100 });";
+echo Html::beginTag('div', ['class' => 'dashboard']);
+$navBar = SubNavBar::begin([
+	'brandLabel' => $object->descriptor,
+	'brandUrl' => $object->getUrl('view'),
+	'options' => [
+		'class' => 'navbar-fixed-top',
+	],
+]);
+$sectionsMenu = [];
+foreach ($sections as $section) {
+	if (substr($section->systemId, 0, 1) === '_') { continue; }
+	$sectionsMenu[] = ['label' => $section->object->sectionTitle, 'url' => '#section-'.$section->systemId];
+}
+echo Html::beginTag('div', ['id' => 'object-dashboard-navbar']);
+echo Nav::widget([
+	'options' => ['class' => 'navbar-nav pull-right'],
+	'items' => $sectionsMenu,
+]);
+echo Html::endTag('div');
+SubNavBar::end();
+
+$grid = Yii::createObject(['class' => 'infinite\web\grid\Grid']);
+$cells = [];
+
+if (isset($sections['_side'])) {
+	$cellInner = $sections['_side']->object->widget;
+	$cellInner->htmlOptions['id'] = $cellInner->id;
+	$js[] = '
+	var $sideBar = $("#'. $cellInner->id .'");
+	$sideBar.affix({
+		\'top\': function () {
+			var offsetTop = $sideBar.offset().top;
+			var sideBarMargin = parseInt($sideBar.children(0).css(\'margin-top\'), 10);
+			var navOuterHeight = $(\'.navbar-header\').height() + $(\'#object-dashboard-navbar\').height();
+			return (this.top = offsetTop - navOuterHeight - sideBarMargin);
+		},
+		\'bottom\': function() {
+			return (this.bottom = $(\'.footer\').outerHeight(true));
+		}
+	});';
+	//$cellInner->htmlOptions['data-spy'] = 'affix';
+	//$cellInner->htmlOptions['data-offset-top'] = 5;
+	// $cellInner->htmlOptions['data-offset-bottom'] = 50;
+	// $cellInner->htmlOptions['data-parent-height-watch'] = true;
+	// $cellInner->htmlOptions['data-parent-height-watch-ancestor'] = '.row';
+	Html::addCssClass($cellInner->htmlOptions, 'ic-sidebar');
+
+	$cells[] = $sideCell = Yii::createObject(['class' => 'infinite\web\grid\Cell', 'content' => $cellInner->generate()]);
+	Yii::configure($sideCell, ['mediumDesktopColumns' => 4, 'largeDesktopSize' => false, 'tabletSize' => false]);
+}
+
+$mainCell = [];
+foreach ($sections as $section) {
+	if (substr($section->systemId, 0, 1) === '_') { continue; }
+	$mainCell[] = $section->object->generate();
+}
+$cells[] = $mainCell = Yii::createObject(['class' => 'infinite\web\grid\Cell', 'content' => implode('', $mainCell)]);
+Yii::configure($mainCell,['mediumDesktopColumns' => 8, 'largeDesktopSize' => false, 'tabletSize' => false]);
+
+$grid->cells = $cells;
+$grid->render();
+echo Html::endTag('div'); // .dashboard
+$this->registerJs(implode("\n", $js));
+?>
