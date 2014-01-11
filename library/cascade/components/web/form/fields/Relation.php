f76e6e0a6377b35d1fdbc1de25f6ba9b82b63be8
Jacob Morrison <jacob@infinitecascade.com>
2014-01-10 19:30:58 -0800

    starting on relation field

--- library/cascade/components/web/form/fields/Relation.php
+++ library/cascade/components/web/form/fields/Relation.php
@@ -2,10 +2,12 @@
 namespace cascade\components\web\form\fields;
 
 use infinite\helpers\ArrayHelper;
+use infinite\helpers\Html;
 use cascade\models\Relation as RelationModel;
 
 class Relation extends Base {
 	public $linkExisting = true;
+	public $linkMultiple = false;
 	public $relatedObject;
 
 	public function init() {
@@ -49,15 +51,35 @@ class Relation extends Base {
 	 * @return unknown
 	 */
 	public function generate() {
-		$companion = $this->modelField->companion;
 		if ($this->linkExisting) {
 			// we are matching with an existing document
-			return 'existing';
+			return $this->generateRelationField();
 		} else {
 			$formSegment = $this->relatedObject->objectType->getFormSegment($this->relatedObject, ['relationField' => $this->modelField]);
 			$formSegment->owner = $this;
 			return $formSegment->generate();
 		}
+	}
+
+	protected function generateRelationField()
+	{
+		$model = $this->model;
+		$companion = $this->modelField->companion;
+		$module = $this->modelField->module;
+		$field = $this->getModelField();
+		$parts = [];
+		$r = [];
+		$r['module'] = $module->systemId;
+		$r['companion'] = $companion->systemId;
+		$r['multiple'] = $this->linkMultiple; // && $this->modelField->relationship->multiple;
+		$r['role'] = $this->modelField->relationship->companionRole($this->modelField->modelRole);
+		if ($this->modelField->baseModel && !$this->modelField->baseModel->isNewRecord) {
+			$r['moduleObjectId'] = $this->modelField->baseModel->primaryKey;
+		}
+		$this->htmlOptions['data-relationship'] = json_encode($r, JSON_FORCE_OBJECT);
+		Html::addCssClass($this->htmlOptions, 'relationship');
+		$parts[] = Html::activeHiddenInput($model, $field, $this->htmlOptions);
+		return implode($parts);
 	}
 }
 ?>
