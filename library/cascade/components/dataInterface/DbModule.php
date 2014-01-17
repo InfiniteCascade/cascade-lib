b31aa37689ea38ea2339538c73f1fc4be7f168a0
Jacob Morrison <jmorrison@psesd.org>
2014-01-17 13:59:08 -0800

    Starting work on interface stuff

--- library/cascade/components/dataInterface/DbModule.php
+++ library/cascade/components/dataInterface/DbModule.php
@@ -1,7 +1,67 @@
 <?php
 namespace cascade\components\dataInterface;
 
+use Yii;
+use infinite\base\exceptions\Exception;
+
+
 abstract class DbModule extends Module {
-	abstract public function getForeignSchema();
+	public $dbConfig = [];
+
+	protected $_models;
+	protected $_db;
+
+	public function getForeignModel($model)
+	{
+		$models = $this->foreignModels;
+		if (isset($models[$model])) {
+			return $models[$model];
+		}
+		return false;
+	}
+
+	public function getForeignModelsConfig()
+	{
+		return [];
+	}
+	
+	public function getForeignModelConfig($modelName)
+	{
+		$config = ['class' => 'cascade\\components\\dataInterface\\connectors\\DbModel'];
+		if (isset($this->foreignModelsConfig[$modelName])) {
+			$config = array_merge($config, $this->foreignModelsConfig[$modelName]);
+		}
+		return $config;
+	}
+
+	public function getForeignModelName($tableName)
+	{
+		return $tableName;
+	}
+	
+	public function getForeignModels()
+	{
+		if (is_null($this->_models)) {
+			foreach ($this->db->schema->findTableNames() as $tableName) {
+				$modelName = $this->getForeignModelName($tableName);
+				$this->_models[$modelName] = Yii::createObject($this->getForeignModelConfig($modelName));
+			}
+		}
+		return $this->_models;
+	}
+	
+	public function getDb()
+	{
+		if (is_null($this->_db)) {
+			if (!isset($this->dbConfig['class'])) {
+				$this->dbConfig['class'] = 'cascade\\components\\dataInterface\\connectors\\DbConnection';
+			}
+			$this->_db = Yii::createObject($this->dbConfig);
+		}
+		if (!$this->_db || !$this->_db->isActive) {
+			throw new Exception("Unable to connect to foreign database.");
+		}
+		return $this->_db;
+	}
 }
 ?>
--- library/cascade/components/dataInterface/connectors/DbConnection.php
+++ library/cascade/components/dataInterface/connectors/DbConnection.php
@@ -1,6 +1,6 @@
 <?php
 namespace cascade\components\dataInterface\connectors;
 
-class DbConnector extends \yii\db\Connection {
+class DbConnection extends \yii\db\Connection {
 }
 ?>
