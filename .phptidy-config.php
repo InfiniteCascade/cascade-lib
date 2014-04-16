9355c9ecf4ba97ec86efacf094ac3b0c50914474
Jacob Morrison <jmorrison@psesd.org>
2014-04-16 15:23:40 -0700

    predocblock

---
+++ .phptidy-config.php
@@ -0,0 +1,24 @@
+<?php
+$default_author = "Jacob Morrison <email@ofjacob.com>";
+
+// Name of the automatically added @package doctag in the phpdoc file docblocks
+// Example: "myproject"
+$default_package = "cascade";
+
+$fix_token_case = false;
+$fix_builtin_functions_case = false;
+$replace_inline_tabs = false;
+$replace_phptags = false;
+$replace_shell_comments = false;
+$fix_statement_brackets = false;
+$fix_separation_whitespace = false;
+$fix_doublearrow_whitespace = false;
+$fix_comma_space = false;
+$add_file_docblock = true;
+$add_function_docblocks = true;
+$add_doctags = true;
+$fix_docblock_format = true;
+$fix_docblock_space = true;
+$add_blank_lines = false;
+$indent = false;
+?>
