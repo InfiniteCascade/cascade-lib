571510953bd3f779e9862726d70625419d5267ae
Jacob Morrison <jomorrison@gmail.com>
2014-07-17 00:06:11 -0500

    Audit Data updates

--- library/cascade/components/db/AuditDataProvider.php
+++ library/cascade/components/db/AuditDataProvider.php
@@ -6,7 +6,10 @@
  */
 
 namespace cascade\components\db;
+
+use Yii;
 use infinite\helpers\ArrayHelper;
+use cascade\models\ObjectFamiliarity;
 
 /**
  * ActiveRecord is the model class for table "{{%active_record}}".
@@ -15,13 +18,65 @@ use infinite\helpers\ArrayHelper;
  */
 class AuditDataProvider extends \infinite\data\ActiveDataProvider
 {
+	public $scope = 'all';
+
+	protected function clearParams(&$params)
+	{
+		foreach ($params as $key => &$value) {
+			if (is_array($value)) {
+				$this->clearParams($value);
+			} elseif ($value === '') {
+				unset($params[$key]);
+			}
+		}
+	}
+
+	public function package()
+	{
+		$package = [];
+		foreach ($this->models as $item) {
+			$eventObject = $item->eventObject;
+			if (emptY($eventObject)) { continue; }
+			$package[$item->primaryKey] = $eventObject->package;
+		}
+		return $package;
+	}
+
 	public function handleInstructions($params)
 	{
+		$this->clearParams($params);
+		//\d($params);
+		$this->scope = ArrayHelper::getValue($params, 'scope', 'watching');
 		$direction = ArrayHelper::getValue($params, 'direction', '_older');
+		$limit = ArrayHelper::getValue($params, 'limit', 50);
 		if ($direction === '_newer') {
-			$lastAge = ArrayHelper::getValue($params, 'loadTimestamp', strtotime("1 year ago"));
+			$lastTime = ArrayHelper::getValue($params, 'loadTimestamp', strtotime("1 year ago"));
+			$this->query->andWhere($this->query->primaryAlias . '.created >= \'' . date("Y-m-d G:i:s", $lastTime) .'\'');
+			$this->query->orderBy([$this->query->primaryAlias . '.created' => SORT_DESC]);
+			$this->pagination->pageSize = false;
+		} else { // _older
+			$this->pagination->pageSize = $limit;
+			$lastTime = ArrayHelper::getValue($params, 'lastItemTimestamp', time());
+			$lastItem = ArrayHelper::getValue($params, 'lastItem', false);
+			$this->query->andWhere($this->query->primaryAlias . '.created <= \'' . date("Y-m-d G:i:s", $lastTime) .'\'');
+			if (!empty($lastItem)) {
+				$this->query->andWhere(['not', [$this->query->primaryAlias . '.' . $this->query->primaryTablePk => $lastItem]]);
+			}
+			$this->query->orderBy([$this->query->primaryAlias . '.created' => SORT_ASC]);
+		}
+
+		if ($this->scope !== 'all' && !empty(Yii::$app->user->id)) {
+			$subquery = ObjectFamiliarity::find();
+			$subquery->andWhere([$subquery->primaryAlias .'.user_id' => Yii::$app->user->id]);
+			if ($this->scope === 'watching') {
+				$subquery->andWhere([$subquery->primaryAlias .'.watching' => 1]);
+			}
+			$subquery->select(['object_id']);
+			$this->query->join('INNER JOIN', ['sof' => $subquery], ['or', '{{sof}}.[[object_id]] = {{'. $this->query->primaryAlias .'}}.[[direct_object_id]]', '{{sof}}.[[object_id]] = {{'. $this->query->primaryAlias .'}}.[[indirect_object_id]]']);
+			$this->query->distinct = true;
+
 		} else {
-			
+			$this->scope = 'all';
 		}
 	}
 }
--- library/cascade/controllers/AppController.php
+++ library/cascade/controllers/AppController.php
@@ -113,16 +113,14 @@ class AppController extends Controller
 
     public function actionActivity()
     {
-        $activity = [];
         Yii::$app->response->baseInstructions['timestamp'] = time();
-        Yii::$app->response->baseInstructions['activity'] = &$activity;
         Yii::$app->response->forceInstructions = true;
         Yii::$app->response->task = 'status';
 
         $auditClass = Yii::$app->classes['Audit'];
         $provider = $auditClass::activityDataProvider();
         $provider->handleInstructions($_POST);
-        \d($provider);
+        Yii::$app->response->baseInstructions['activity'] = $provider->package();
     }
 
     /**
