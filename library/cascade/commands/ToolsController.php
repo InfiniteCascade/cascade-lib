8cb8bb8b29b14f33817b082a8f732f2bd48297b2
Jake Oehler Morrison <jmorrison@psesd.org>
2015-03-04 12:20:36 -0800

    working on fixer

--- library/cascade/commands/ToolsController.php
+++ library/cascade/commands/ToolsController.php
@@ -37,30 +37,79 @@ class ToolsController extends \infinite\console\Controller
         $this->out("Done!");
     }
 
-    public function actionFixSlashes()
+    public function actionFixProject()
     {
-        $dirs = [Yii::getAlias('@cascade'), Yii::getAlias('@infinite'), Yii::getAlias('@cascade/modules/core'), Yii::getAlias('@cascade/modules/core'), Yii::getAlias('@infinite/deferred'), Yii::getAlias('@psesd/cascade')];
+        $dirs = [
+            Yii::getAlias('@cascade'), 
+            Yii::getAlias('@infinite'), 
+            Yii::getAlias('@cascade/modules/core'), 
+            Yii::getAlias('@psesd/cascade'),
+            Yii::getAlias('@infinite/deferred'), 
+            Yii::getAlias('@infinite/notification'), 
+        ];
+        $customStart = microtime(true);
+        Console::stdout("Running custom fixes...".PHP_EOL);
         foreach ($dirs as $dir) {
             $dirStart = microtime(true);
             $changed = 0;
-            Console::stdout($dir ."...");
+            Console::stdout("\t" . $dir ."...");
             $files = FileHelper::findFiles($dir, ['only' => ['*.php'], 'recursive' => true]);
             Console::stdout("found " .count($files)." files...");
             foreach ($files as $file) {
-                if ($this->fixFileSlashes($file)) {
+                if ($this->fixFile($file)) {
                     $changed++;
                 }
             }
             Console::stdout("changed {$changed} files in ". round(microtime(true)-$dirStart,1)."s!".PHP_EOL);
         }
+        Console::stdout("done in ". round(microtime(true)-$customStart,1)."s!".PHP_EOL.PHP_EOL);
+
+
+        $phpcsStart = microtime(true);
+        Console::stdout("Running custom fixes...".PHP_EOL);
+        foreach ($dirs as $dir) {
+            $dirStart = microtime(true);
+            $changed = 0;
+            Console::stdout("\t" . $dir ."...");
+            $configFiles = [];
+            $configFiles[] = $dir . DIRECTORY_SEPARATOR . '.php_cs';
+            $configFiles[] = dirname($dir) . DIRECTORY_SEPARATOR . '.php_cs';
+            $configFiles[] = dirname(dirname($dir)) . DIRECTORY_SEPARATOR . '.php_cs';
+            $foundConfig = false;
+            foreach ($configFiles as $configFile) {
+                if (file_exists($configFile)) {
+                    $foundConfig = $configFile;
+                    break;
+                }
+            }
+            if (!$foundConfig) {
+                Console::stdout("skipped!" . PHP_EOL);
+                continue;
+            }
+            Console::stdout($foundConfig);
+            
+            Console::stdout("done in ". round(microtime(true)-$dirStart,1)."s!".PHP_EOL);
+        }
+        Console::stdout("done in ". round(microtime(true)-$phpcsStart,1)."s!".PHP_EOL.PHP_EOL);
     }
 
-    public function fixFileSlashes($file)
+    public function fixFile($file)
     {
         $contents = preg_split("/\\r\\n|\\r|\\n/", file_get_contents($file));
         $changed = false;
-        foreach ($contents as $lineNumber => $line) {
+        if ($this->fixFileSlashes($file, $contents)) {
+            $changed = true;
+        }
+        if ($changed) {
+            file_put_contents($file, implode("\n", $contents));
+        }
+        return $changed;
+    }
 
+    public function fixFileSlashes($file, &$contents)
+    {
+        $changed = false;
+        foreach ($contents as $lineNumber => $line) {
             if (preg_match('/(psesd|infinite|cascade)\\\\\\\/', $line) === 1) {
                 $fixedLine = preg_replace('/\\\\\\\/', '\\', $line);
                 if ($fixedLine !== $line) {
@@ -68,9 +117,6 @@ class ToolsController extends \infinite\console\Controller
                     $changed = true;
                 }
             }
-        }
-        if ($changed) {
-            file_put_contents($file, implode("\n", $contents));
         }
         return $changed;
     }
