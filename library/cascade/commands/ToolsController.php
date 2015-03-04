76dcdabe456df75cba20602c48ed957bfc998a29
Jake Oehler Morrison <jmorrison@psesd.org>
2015-03-04 15:17:15 -0800

    working on doc fixes

--- library/cascade/commands/ToolsController.php
+++ library/cascade/commands/ToolsController.php
@@ -1,9 +1,8 @@
 <?php
 /**
  * @link http://www.infinitecascade.com/
- *
- * @copyright Copyright (c) 2014 Infinite Cascade
- * @license http://www.infinitecascade.com/license/
+ * @copyright Copyright (c) 2015 Infinite Cascade
+ * @license http://www.infinitecascade.com/license
  */
 
 namespace cascade\commands;
@@ -22,7 +21,6 @@ class ToolsController extends \infinite\console\Controller
 {
     /**
      * __method_actionFlush_description__.
-     *
      * @param __param_category_type__ $category __param_category_description__ [optional]
      */
     public function actionFlush($category = null)
@@ -39,6 +37,9 @@ class ToolsController extends \infinite\console\Controller
         $this->out("Done!");
     }
 
+    /**
+     * [[@doctodo method_description:actionFixProject]]
+     */
     public function actionFixProject()
     {
         $dirs = [
@@ -107,11 +108,19 @@ class ToolsController extends \infinite\console\Controller
         Console::stdout("done in " . round(microtime(true)-$phpcsStart, 1) . "s!" . PHP_EOL . PHP_EOL);
     }
 
+    /**
+     * [[@doctodo method_description:fixFile]]
+     * @param __param_file_type__ $file __param_file_description__
+     * @return __return_fixFile_type__ __return_fixFile_description__
+     */
     public function fixFile($file)
     {
         $contents = preg_split("/\\r\\n|\\r|\\n/", file_get_contents($file));
         $changed = false;
         if ($this->fixFileSlashes($file, $contents)) {
+            $changed = true;
+        }
+        if ($this->fixDocBlockPlaceholder($file, $contents)) {
             $changed = true;
         }
         if ($changed) {
@@ -121,6 +130,27 @@ class ToolsController extends \infinite\console\Controller
         return $changed;
     }
 
+    public function fixDocBlockPlaceholder($file, &$contents)
+    {
+        $changed = false;
+        foreach ($contents as $lineNumber => $line) {
+            $line = trim($line);
+            if (substr($line, 0, 1) === '*' || substr($line, 0, 1) === '/') {
+                if (strpos($line, '__') === false) {
+                    unset($contents[$lineNumber]);
+                    $changed = true;
+                }
+            }
+        }
+        return $changed;
+    }
+
+    /**
+     * [[@doctodo method_description:fixFileSlashes]]
+     * @param __param_file_type__ $file __param_file_description__
+     * @param __param_contents_type__ $contents __param_contents_description__
+     * @return __return_fixFileSlashes_type__ __return_fixFileSlashes_description__
+     */
     public function fixFileSlashes($file, &$contents)
     {
         $changed = false;
