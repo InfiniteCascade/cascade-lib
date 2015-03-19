d3518783238bb690dbc6fce927e28e50b3dad43b
Jake Oehler Morrison <jmorrison@psesd.org>
2015-03-18 18:43:23 -0700

    setup update

--- lib/setup/Setup.php
+++ lib/setup/Setup.php
@@ -9,22 +9,10 @@
 namespace cascade\setup;
 
 /**
- * Setup [[@doctodo class_description:cascade\setup\Setup]].
+ * Setup Perform the web setup for the application.
  *
  * @author Jacob Morrison <email@ofjacob.com>
  */
 class Setup extends \canis\setup\Setup
 {
-    /**
-     * @inheritdoc
-     */
-    public static function createSetupApplication($config = [])
-    {
-        if (is_null(self::$_instance)) {
-            $className = __CLASS__;
-            self::$_instance = new $className($config);
-        }
-
-        return parent::createSetupApplication($config);
-    }
 }
